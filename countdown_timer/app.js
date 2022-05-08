50aa12666421ff5564fc1e3585773cf19cea883a
Jaroslaw Charchula <charchula@gmail.com>
2022-05-08 19:36:49 +0200

    finish js logic with countdown. finiseh countdown_timer project.

--- countdown_timer/app.js
+++ countdown_timer/app.js
@@ -26,7 +26,7 @@ const giveaway = document.querySelector('.giveaway');
 const deadline = document.querySelector('.deadline');
 const items = document.querySelectorAll('.deadline-format h4');
 
-let futureDate = new Date(2022, 4, 20, 11, 30, 0);
+let futureDate = new Date(2023, 4, 08, 19, 30, 0);
 
 
 const year = futureDate.getFullYear();
@@ -57,18 +57,36 @@ function getRemaningTime() {
   // values in miliseconds
   const oneDay = 24*60*60*1000;
   const oneHour = 60*60*1000;
-  const oneMinute = 60*100;
+  const oneMinute = 60*1000;
   
   //calculate all values
-  let days =Math.floor.apply(t/oneDay);  
+  let days =Math.floor(t/oneDay);  
   let hours = Math.floor((t % oneDay) / oneHour);  
   let minutes = Math.floor((t % oneHour) / oneMinute);
-  let seconds = Math.floor((t % oneMinute) / 1000);
-  console.log(seconds);
+  let seconds = Math.floor((t % oneMinute) / 1000);  
   
   //set values array
-  const values = [days,hours,minutes,seconds];
+  const values = [days, hours, minutes, seconds];
+
+  function format(item) {
+    if(item < 10) {
+      return item = `0${item}` 
+    }
+    return item
+  }
+
+  items.forEach((item, index) => {
+    item.innerHTML = format(values[index]);
+  });
+  if(t < 0) {
+    clearInterval(countdown);
+    deadline.innerHTML = `<h4 class="expired">sorry, this giveaway has expired</h4>`;
+  }
 }
+
+//countdown
+let countdown = setInterval(getRemaningTime, 1000);
+
 
 getRemaningTime();
 
